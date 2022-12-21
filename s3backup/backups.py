afae1b702dc6a104970a27741764e73a6a67b214
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-21 03:18:19 +0000

    Adding tarfile member sanitization to extractall()

--- s3backup/backups.py
+++ s3backup/backups.py
@@ -216,7 +216,26 @@ class BackupHandler(object):
             directory = os.path.dirname(directory)
 
         with tarfile.open(tarpath) as f:
-            f.extractall(directory)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, directory)
 
     def _get_dirname(self, path):
         expanded = os.path.expanduser("{}/".format(path))
